b21a3698d2d923324e057057bf9e7260f37d1c67
Nik Logan <nicholasklogan@gmail.com>
2022-12-11 18:02:50 -0500

    Red test_direct_conflict_create_appointment_doctor_who
    Exception:
    
    >       assert response.status_code == HTTPStatus.CONFLICT
    E       assert 200 == <HTTPStatus.CONFLICT: 409>
    E         +200
    E         -<HTTPStatus.CONFLICT: 409>
    
    tests\test_home.py:41: AssertionError

--- tests/test_home.py
+++ tests/test_home.py
@@ -21,6 +21,25 @@ def test_successfully_create_appointment_doctor_who(client):
     assert db_value.start_time == json_value['start_time']
     assert db_value.end_time == json_value['end_time']
 
+def test_direct_conflict_create_appointment_doctor_who(client):
+    # Setup
+    json_value = {
+        "doctor": "Who",
+        "start_time": datetime.datetime(year=2022, month=12, day=11, hour=13, minute=0, second=0).timestamp(),
+        "end_time": datetime.datetime(year=2022, month=12, day=11, hour=13, minute=30, second=0).timestamp()
+    }
+
+    response = client.post(
+        "/schedule_appointment",
+        json=json_value
+    )
+    assert response.status_code == HTTPStatus.OK
+    response = client.post(
+        "/schedule_appointment",
+        json=json_value
+    )
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert len(Appointment.query.filter_by(**json_value).all()) == 1
 
 
 def test_home_api(client):
