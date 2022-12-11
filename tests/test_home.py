bf3cbd8a7ebc222374bf1ac78dc976e507274cff
Nik Logan <nicholasklogan@gmail.com>
2022-12-11 17:18:40 -0500

    Red test_successfully_create_appointment_doctor_who
    Exception:
    
    >       assert response.status_code == HTTPStatus.OK
    E       assert 404 == <HTTPStatus.OK: 200>
    E         +404
    E         -<HTTPStatus.OK: 200>
    
    tests\test_home.py:14: AssertionError

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,4 +1,19 @@
+import datetime
 from http import HTTPStatus
+
+def test_successfully_create_appointment_doctor_who(client):
+    json_value = {
+        "doctor": "Who",
+        "start_time": datetime.datetime(year=2022, month=12, day=11, hour=13, minute=0, second=0).timestamp(),
+        "end_time": datetime.datetime(year=2022, month=12, day=11, hour=13, minute=30, second=0).timestamp()
+    }
+    response = client.post(
+        "/schedule_appointment",
+        json=json_value
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json == json_value
+
 
 
 def test_home_api(client):
